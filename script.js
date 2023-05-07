5a62351127eed2dd1fcf5924b400e7f555d4d116
Leo <changliyao19@gmail.com>
2023-05-06 22:46:12 -0400

    added starter code to main branch

---
+++ script.js
@@ -0,0 +1,12 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Generate a random password based on user criteria
